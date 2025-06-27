808b08da2de2b8ac5f6f69f06415781bea831b66
Far <lqdevelop01@gmail.com>
2025-06-27 17:11:16 -0400

    tabela de produtos

--- app/views/produtos.php
+++ app/views/produtos.php
@@ -16,28 +16,43 @@
                     <button id="btn_cadastrar_produto">Cadastrar Produto</button>
                 </div>
         </div>
-            <div id="produtos_listaProdutos">
-                <table>
-                    <thead>
-                        <!--O cabeçalho deve permanecer fixo, mas a tabela deve ser rolável,
-                        configurar isso depois-->
-                        <tr>
-                            <th>ID</th>
-                            <th>Cód. Barras</th>
-                            <th>Nome</th>   
-                            <th>Estoque</th>
-                            <th>Valor</th>
-                    </thead>
-                    <tbody id="tabela_produtos">
-                        <!-- TODO: Os produtos serão inseridos aqui dinamicamente, configurar isso mais tarde 
-                        Devo criar uma classe pra gerar diferentes tabelas de produtos para cada view?-->
-                    </tbody>
-                </table>
+            <div id="produtos_areaListagem_containerLista">
+                <div id="produtos_listaProdutos">
+                    <table id="tabela_lista_produtos">
+                        <thead id="lista_produtos_header">
+                            <!--O cabeçalho deve permanecer fixo, mas a tabela deve ser rolável,
+                            configurar isso depois-->
+                            <tr>
+                                <th>ID</th>
+                                <th>Cód. Barras</th>
+                                <th>Nome</th>   
+                                <th>Estoque</th>
+                                <th>Valor</th>
+                        </thead>
+                        <tbody id="tabela_produtos">
+                            <!-- TODO: Os produtos serão inseridos aqui dinamicamente, configurar isso mais tarde 
+                            Devo criar uma classe pra gerar diferentes tabelas de produtos para cada view?-->
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     <div id="produtos_areaExibicao">
         <div id="produtos_areaExibicao_detalhes">
-            <div id="produtos_areaExibicao_detalhes_areaDadosProduto"></div>
+            <div id="produtos_areaExibicao_detalhes_areaDadosProduto">
+                <h4 class="titulo_discreto">PRODUTO</h4>
+                <h1 id="lbl_ID_produto">1</h1>
+                <h2 class="lbl_info_produto" id="lbl_nome_produto">COCA-COLA ZERO 2L</h2>
+                <h4 class="titulo_discreto">CÓDIGO DE BARRAS</h4>
+                <h3 class="lbl_info_produto" id="lbl_codigo_barras">7891234567890</h1>
+                <h4 class="titulo_discreto">VALOR UNITÁRIO</h4>
+                <h3 class="lbl_info_produto" id="lbl_valor_unitario">R$ 12,00</h1>
+                <h4 class="titulo_discreto">ESTOQUE ATUAL</h4>
+                <h2 class="lbl_info_produto" id="lbl_estoque_atual">100 un</h1>
+
+
+
+            </div>
             <div id="produtos_areaExibicao_detalhes_areaImagemProduto">
                 <img id="produtos_imagem_produto" src="assets/img/image.png" height="300" width="300">
             </div>
